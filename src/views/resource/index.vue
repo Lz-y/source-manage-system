a7d195b21f3add0dc2b73e9db2e5c1285dfbfe2a
lzy <2397925443@qq.com>
2022-02-17 21:59:51 +0800

    feat: create new resource form

--- src/views/resource/index.vue
+++ src/views/resource/index.vue
@@ -2,7 +2,7 @@
   <div class="resource-manage-page">
     <Query :configs='configs' :data='queryData' size='small' inline>
       <el-button type='primary' :icon='Search'>查询</el-button>
-      <el-button :icon='CirclePlus'>添加资源</el-button>
+      <el-button :icon='CirclePlus' @click="addResource">添加资源</el-button>
     </Query>
     <CustomTable :columns='columns' :data='list'>
       <template #img='{row}'>
@@ -42,6 +42,28 @@
         <el-button size='small' type="primary" @click="confirm">确认</el-button>
       </template>
     </el-dialog>
+    <el-dialog v-model="showAddResource" title="添加资源" width="35%">
+      <el-row>
+        <el-col :xs="4" :sm="6" :md="6">
+          <el-upload
+            class="avatar-upload"
+            action=""
+            :show-file-list="false"
+            :on-success="uploadSuccess"
+            :before-upload="beforeUpload">
+            <img :src="resource.img" alt="avatar" v-if="resource.img" class="avatar">
+            <el-icon v-else class="avatar-upload-icon"><plus /></el-icon>
+          </el-upload>
+        </el-col>
+        <el-col :xs="20" :sm="18" :md="16">
+          <Query ref="userForm$" :configs="resourceConfig" :data="resource" :rules="createResourceRules" label-width="80px" size="small" status-icon></Query>
+        </el-col>
+      </el-row>
+      <template #footer>
+        <el-button size="mini" @click="cancelAdd">取消</el-button>
+        <el-button type="primary" size="mini" @click="saveResource">保存</el-button>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
@@ -51,8 +73,8 @@ export default {
 }
 </script>
 <script setup lang="ts">
-import {ref, reactive} from 'vue-demi'
-import { Search, CirclePlus, InfoFilled } from '@element-plus/icons-vue'
+import {ref, reactive, nextTick} from 'vue-demi'
+import { Search, CirclePlus, InfoFilled, Plus } from '@element-plus/icons-vue'
 
 import Query from '@/components/query.vue'
 import CustomTable from '@/components/table/index.vue'
@@ -82,7 +104,7 @@ const columns = ref<Array<ColumnProps>>([
   { attrs: { prop: 'img', label: '封面' }, _slot: true },
   { attrs: { prop: 'url', label: '链接' }, _slot: true },
   { attrs: { prop: 'classify', label: '所属类别' }, _slot: true },
-  { attrs: { prop: 'description', label: '描述' } },
+  { attrs: { prop: 'description', label: '简介' } },
   { attrs: { prop: 'encrypt', label: '是否加密' }, _slot: true },
   { attrs: { prop: 'status', label: '资源状态' }, _slot: true},
   { attrs: { prop: 'operation', label: '操作', width: 120 }, _slot: true },
@@ -103,6 +125,22 @@ const rules = reactive({
     {validator: validatorPsw, trigger: ['change', 'blur']},
   ]
 })
+
+const showAddResource = ref<boolean>(false)
+const resource = reactive<Partial<ResourceFile>>({
+  status: 1
+})
+const createResourceRules = reactive({
+  name: [{required: true, message: '请输入资源名称', trigger: 'blur'}],
+  classify: [{required: true, message: '请选择分类', trigger: 'blur'}],
+})
+const resourceConfig = ref<Array<QConfig>>([
+  {name: 'input', prop: 'name', label: '名称', attrs: {placeholder: '请输入资源名称', clearable: true}},
+  {name: 'select', prop: 'classify', label: '分类', attrs: {placeholder: '请选择资源分类'}},
+  {name: 'input', prop: 'description', label: '简介', attrs: {type: 'textarea', placeholder: '请输入简介'}},
+  {name: 'switch', prop: 'encrypt', label: '是否加密', attrs: {}, events: {change: switchEncrypt}},
+  {name: 'select', prop: 'status', label: '状态', attrs: {placeholder: '请选择资源状态', clearable: true}, options: [{label: '已失效', value: 0}, {label: '正常', value: 1}]}
+])
 function validatorPsw (rule: any, value: string, callback: Function) {
   if (!/^[a-zA-Z0-9\.@#]{4,8}$/g.test(value)) {
     callback(new Error('仅能输入4-8位大小写字母、数字和.#@'))
@@ -110,6 +148,40 @@ function validatorPsw (rule: any, value: string, callback: Function) {
     callback()
   }
 }
+function addResource() {
+  showAddResource.value = true
+}
+
+function beforeUpload () {
+
+}
+
+function uploadSuccess () {
+
+}
+
+function switchEncrypt (state: boolean) {
+  nextTick(() => {
+    if (state) {
+      resourceConfig.value.splice(4, 0, {name: 'input', prop: 'psw', label: '密码', attrs: {type: 'password', placeholder: '请输入密码'}})
+      Object.assign(createResourceRules,{psw: [
+        {required: true, message: '请输入文件加密密码', trigger: 'blur'},
+        {validator: validatorPsw, trigger: ['change', 'blur']},
+      ]})
+    } else {
+      resourceConfig.value.splice(4, 1)
+    }
+  })
+}
+
+function cancelAdd () {
+  showAddResource.value = false
+}
+
+function saveResource () {
+  showAddResource.value = false
+}
+
 function showEncryptDislog () {
   visible.value = true
 }
@@ -123,3 +195,39 @@ function confirm () {
   visible.value = false
 }
 </script>
+
+<style lang="scss">
+.resource-manage-page{
+  .avatar-upload {
+    width: 100%;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+
+    .el-upload{
+      width: 100%;
+      &:hover{
+        border-color: #409eff;
+      }
+    }
+
+    &-icon {
+      border-radius: 6px;
+      border: 1px dashed #d9d9d9;
+      font-size: 28px;
+      color: #8c939d;
+      width: 100%;
+      height: 178px;
+      line-height: 178px;
+      text-align: center;
+      box-sizing: border-box;
+    }
+    .avatar {
+      width: 178px;
+      height: 178px;
+      display: block;
+      object-fit: cover;
+    }
+  }
+}
+</style>
